7c5d5ee8017f0b8f4769384fba0fb40779e078d3
admin <admin@JSTUSER>
2017-12-06 09:42:54 +0800

    xml

--- SerializationTest/SerializationTest/Program.cs
+++ SerializationTest/SerializationTest/Program.cs
@@ -10,65 +10,58 @@ namespace SerializationTest
         static void Main(string[] args)
         {
             //function.XMLSerialize();
-            function.SerializeNow();
-            function.DeSerializeNow();
+            function.XMLDeserialize();
+            //function.SerializeNow();
+            //function.DeSerializeNow();
         }
     }
     public class function
     {
-        public static void SerializeNow()
-        {
-            ClassToSerialize c = new ClassToSerialize();
-            FileStream fileStream =
-            new FileStream("e:\\temp.dat", FileMode.Create);
-            BinaryFormatter b = new BinaryFormatter();
-            b.Serialize(fileStream, c);
-            fileStream.Close();
-            Console.WriteLine("二进制序列化完成。。。");
-        }
-        public static void DeSerializeNow()
-        {
-            Console.WriteLine("开始二进制反序列化序列化");
-            ClassToSerialize c = new ClassToSerialize();
-            //c.Sex = "kkkk";
-            FileStream fileStream =
-            new FileStream("e:\\temp.dat",
-            FileMode.Open, FileAccess.Read, FileShare.Read);
-            BinaryFormatter b = new BinaryFormatter();
-            c = b.Deserialize(fileStream) as ClassToSerialize;
-            Console.WriteLine(c.id);
-            Console.WriteLine(c.name);
-            Console.WriteLine(c.Sex);
-            fileStream.Close();
-            Console.Read();
-        }
+        //public static void SerializeNow()
+        //{
+        //    ClassToSerialize c = new ClassToSerialize();
+        //    FileStream fileStream =
+        //    new FileStream("e:\\temp.dat", FileMode.Create);
+        //    BinaryFormatter b = new BinaryFormatter();
+        //    b.Serialize(fileStream, c);
+        //    fileStream.Close();
+        //    Console.WriteLine("二进制序列化完成。。。");
+        //}
+        //public static void DeSerializeNow()
+        //{
+        //    Console.WriteLine("开始二进制反序列化序列化");
+        //    ClassToSerialize c = new ClassToSerialize();
+        //    //c.Sex = "kkkk";
+        //    FileStream fileStream =
+        //    new FileStream("e:\\temp.dat",
+        //    FileMode.Open, FileAccess.Read, FileShare.Read);
+        //    BinaryFormatter b = new BinaryFormatter();
+        //    c = b.Deserialize(fileStream) as ClassToSerialize;
+        //    Console.WriteLine(c.id);
+        //    Console.WriteLine(c.name);
+        //    Console.WriteLine(c.Sex);
+        //    fileStream.Close();
+        //    Console.Read();
+        //}
 
-        //public static void XMLSerialize()
-        //{
-        //    Person c = new Person("cyj");
-        //    c.Courses = new Course[2];
-        //    c.Courses[0] = new Course("英语", "交流工具");
-        //    c.Courses[1] = new Course("数学", "自然科学");
-        //    XmlSerializer xs = new XmlSerializer(typeof(Person));
-        //    Stream stream = new FileStream("e:\\cyj.xml", FileMode.Create, FileAccess.Write, FileShare.Read);
-        //    xs.Serialize(stream, c);
-        //    stream.Close();
-        //}
-        //public void XMLDeserialize()
-        //{
-        //    XmlSerializer xs = new XmlSerializer(typeof(Person));
-        //    Stream stream = new FileStream("C:\\cyj.XML", FileMode.Open, FileAccess.Read, FileShare.Read);
-        //    Person p = xs.Deserialize(stream) as Person;
-        //    Response.Write(p.Name);
-        //    Response.Write(p.Age.ToString());
-        //    Response.Write(p.Courses[0].Name);
-        //    Response.Write(p.Courses[0].Description);
-        //    Response.Write(p.Courses[1].Name);
-        //    Response.Write(p.Courses[1].Description);
-        //    stream.Close();
-        //}
+        public static void XMLSerialize()
+        {
+            Person c = new Person("cyj");
+            c.Courses = new Course[2];
+            c.Courses[0] = new Course("英语", "交流工具");
+            c.Courses[1] = new Course("数学", "自然科学");
+            XmlSerializer xs = new XmlSerializer(typeof(Person));
+            Stream stream = new FileStream("e:\\cyj.m", FileMode.Create, FileAccess.Write, FileShare.Read);
+            xs.Serialize(stream, c);
+            stream.Close();
+        }
+        public static void XMLDeserialize()
+        {
+            XmlSerializer xs = new XmlSerializer(typeof(DOModule));
+            Stream stream = new FileStream("E:\\abc.xml", FileMode.Open, FileAccess.Read, FileShare.Read);
+            DOModule m = xs.Deserialize(stream) as DOModule;
+        }
     }
-
     [Serializable]
     public class ClassToSerialize
     {
@@ -106,6 +99,7 @@ namespace SerializationTest
             Sex = "男";
         }
     }
+
     [Serializable]
     public class Course
     {
@@ -119,6 +113,22 @@ namespace SerializationTest
         {
             Name = name;
             Description = description;
+        }
+    }
+
+    [Serializable]
+    [XmlRoot("DOModule")]
+    public class DOModule
+    {
+        public DOModule()
+        {
+        }
+        private string doobjects;
+        [XmlElement(ElementName = "DOObjects")]
+        public string DOObjects
+        {
+            get { return doobjects; }
+            set { doobjects = value; }
         }
     }
 }
